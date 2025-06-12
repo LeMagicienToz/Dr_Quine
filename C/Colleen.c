73b2784611017807c6775abfbb402c524f1f3913
LeMagicienToz <maxouiii4.uteza@gmail.com>
2025-06-12 17:44:25 +0200

    Collen in c Done

--- C/Colleen.c
+++ C/Colleen.c
@@ -10,11 +10,12 @@ int called_function()
 
 int main ()
 {
-    char *s = "#include <stdio.h>%c/*%c    Comment Outside the Function%c*/%c";
+    //    */%c    char *s = %c%s%c;%c    printf(s,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10);%c    called_function();%c    return (0);%c}%c";
     /*
         Comment in the Function 
-    */
-    printf("%s", "")
+    */ 
+    char *s = "#include <stdio.h>%c/*%c    Comment Outside the Function%c*/%c%cint called_function()%c{%c    return (0);%c}%c%cint main ()%c{%c    /*%c       Comment in the Function %c    */%c    char *s=%c%s%c;%c    printf(s,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10,10);%c    called_function();%c    return (0);%c}";
+    printf(s,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10,10);
     called_function();
     return (0);
 }
